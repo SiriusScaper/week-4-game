8d9841148aad418b9007c1ff85384f43e2320f71
SiriusScaper <siriusscaper@gmail.com>
2018-02-13 13:56:31 -0800

    Added random number function and other things that don't work

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,9 +2,30 @@ console.log('Hello World')
 
 //Global variables
 
-//Wins
-//Losses
+let buttonNumbers = [];
+let minNum = randomNumRange(1, 12);
+let maxNum = randomNumRange(19, 120);
+let wins
+let losses
 
-function randomNum() {
-    
+
+//Generate a random number using arguements to get specific ranges for the elements of the game
+function randomNumRange(min, max) {
+    return Math.floor(Math.random()*(max - min + 1)) + min;
 }
+
+//Function to add main random number to dom
+function addToHTML(){
+    $('#whatever').html(maxNum.attr('value'));
+}
+
+
+console.log("poop " + minNum + ' ' + maxNum);
+
+// function genRanNumButtons() {
+//     while (buttonNumbers.length < 4){
+//         buttonNumbers.push(storeNum);
+//     }
+// }
+
+// console.log(buttonNumbers)
