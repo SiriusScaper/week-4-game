4cd30a1801aa529451e6169ac36619a87cb5642c
SiriusScaper <siriusscaper@gmail.com>
2018-06-30 12:45:41 -0700

    Updated to reflect new random array of numbers and display information to user

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,15 +8,7 @@ let maxNum = randomNumRange(19, 120);
 let wins = 0;
 let losses = 0;
 let score = 0;
-const btnNum = [];
-
-// reset minNum/maxNum values
-
-const reset = () => {
-  maxNum = randomNumRange(19, 120);
-  minNum = randomNumRange(1, 12);
-  score = 0;
-};
+let btnNum = [];
 
 // Update array with four random numbers to use on the "buttons"
 const numArrUpdate = () => {
@@ -26,6 +18,16 @@ const numArrUpdate = () => {
   }
 };
 numArrUpdate();
+
+// reset minNum/maxNum values
+
+const reset = () => {
+  btnNum = [];
+  numArrUpdate();
+  maxNum = randomNumRange(19, 120);
+  score = 0;
+};
+
 
 // Need a function to compare maxNum to current score value
 const compare = () => {
@@ -44,7 +46,6 @@ const compare = () => {
 console.log(btnNum);
 
 // Function to add main random number to dom
-$('#bigrandom').html(maxNum);
 
 
 // Add click functionality to "buttons"
@@ -76,10 +77,13 @@ $('#violet').on('click', () => {
 });
 
 // Need a function to add score number to dom
+$('#bigrandom').html(maxNum);
+
 $('#red, #blue, #green, #violet').click(() => {
   $('#score').html(score);
   $('#wins').html(wins);
   $('#losses').html(losses)
+  $('#bigrandom').html(maxNum);
 });
 
 // const check = (compare) => {
